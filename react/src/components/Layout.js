7ac372a0e04404e04d29edfcf037320d92563439
Jenn Eng <jenneng6@gmail.com>
2017-02-01 17:08:16 -0500

    Added Home link and Back button

--- react/src/components/Layout.js
+++ react/src/components/Layout.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router';
+import BackButton from './BackButton.js';
 
 const Layout = (props) => {
   return(
     <div>
+      <BackButton />
       <span>
-        When I grow up, I will be a dope header. 
+        When I grow up, I will be a dope header.
       </span>
+      <br />
+      <Link to='/'> HOME </Link>
+      <Link to='/'>  </Link>
       <br />
       <h1 className="page-title"> Bars </h1>
       { props.children }
